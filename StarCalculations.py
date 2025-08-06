6f5cc02c892a62931d65cbc5d4b15ebf39d1ef51
AndrewASO <andrew.skevington-24@ncf.edu>
2025-08-06 00:18:27 -0400

    Implemented calculateColor function, but might optimize it in the future to be more dynamic rather than having a bunch of if/elif statements checking to see if it falls within a range to be a type of star

--- StarCalculations.py
+++ StarCalculations.py
@@ -14,5 +14,26 @@ class StarCalculations:
     def calculateSize(self):
         pass
 
-    def calculateColor(self):
-        pass
+
+    '''
+    Takes in a flaot value for ci (color index) that would be given as blue magnitude - visual magnitude from a distance of 10 parsecs.
+    The goal of this function is to see what range the ci falls within and then return it as an RGB value that the human eye can
+    visualize.
+    Returns color (triple float list)
+    '''
+    def calculateColor(self, ci):
+        
+        #Convert B-V color index to RGB using temperature approximation 
+
+        if ci < -0.3:                   #Extremely blue (0/B Stars)
+            return (0.6, 0.7, 0.8) 
+        elif ci < 0.0:                  #Blue-white (A Stars)
+            return (0.8, 0.9, 1.0)
+        elif ci < 0.3:                  #White (F Stars)
+            return (1.0, 1.0, 1.0)
+        elif ci < 0.6:                  #Yellow-white (G Stars)
+            return (1.0, 1.0, 0.8)
+        elif ci < 1.0:                  #Orange (K Stars)
+            return (1.0, 0.8, 0.6)
+        else:                           #Red (M Stars)
+            return (1.0, 0.6, 0.4)
